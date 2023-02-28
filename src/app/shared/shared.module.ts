7b0ca3c4646011f71616a5e334de94981a68b259
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2023-02-28 14:51:45 +0530

    changes

--- src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PrimengModule } from './primeNG/primeng/primeng.module';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, PrimengModule],
-})
-export class SharedModule {}
